d0e1d4a1a25147044620256766c16b2bf5928337
Hrant <harvey.mirijanyan@gmail.com>
2017-04-25 15:42:36 -0700

    Add animal class

---
+++ BuildingBlocks/src/Animal.java
@@ -0,0 +1,12 @@
+
+public class Animal {
+	String name;
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String newName){
+		name = newName;
+	}
+}
--- BuildingBlocks/src/FirstClass.java
+++ BuildingBlocks/src/FirstClass.java
@@ -1,9 +1,11 @@
 
 public class FirstClass {
-
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		Animal dog = new Animal();
+		dog.setName("Fredo");
+		System.out.println(dog.getName());
 
 	}
 
