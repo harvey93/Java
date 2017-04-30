6f02540ed2e0b80407bf7951235560b0782933c1
Hrant <harvey.mirijanyan@gmail.com>
2017-04-30 08:54:14 -0700

    Sort ArrayList

--- CoreJavaAPI/src/myArrayList/myArrayList.java
+++ CoreJavaAPI/src/myArrayList/myArrayList.java
@@ -12,5 +12,25 @@ public class myArrayList {
 		list2.add("bluejay");
 		list2.add(1, "pidgeon");
 		System.out.println(list2);
+		list2.remove("eagle");
+		System.out.println(list2);
+		list2.remove(1);
+		System.out.println(list2);
+		list2.set(1, "newestBird");
+		System.out.println(list2);
+		System.out.println(list2.get(1));
+		System.out.println(list2.isEmpty());
+		System.out.println(list2.contains("Sparrow"));
+		list1.add("Sparrow");
+		list1.add("newestBird");
+		System.out.println(list2.equals(list1));
+		list3.add(5);
+		list3.add(1);
+		list3.add(3);
+		list3.add(8);
+		list3.add(0);
+		System.out.println(list3);
+		Collections.sort(list3);
+		System.out.println(list3);
 	}
 }
