744d3ae1c736c6da1b972985aee24cf7592040f9
Hrant <harvey.mirijanyan@gmail.com>
2017-04-30 08:02:09 -0700

    Add Array Lists

---
+++ CoreJavaAPI/src/myArrayList/myArrayList.java
@@ -0,0 +1,15 @@
+package myArrayList;
+import java.util.*;
+
+public class myArrayList {
+	
+	public static void main(String[] args){
+		ArrayList list1 = new ArrayList();
+		ArrayList<String> list2 = new ArrayList<String>();
+		ArrayList<Integer> list3 = new ArrayList<>();
+		list3.add(5);
+		list3.add(6);
+//		System.out.println(ArrayList.toString(list3));
+		System.out.println(list3);
+	}
+}
