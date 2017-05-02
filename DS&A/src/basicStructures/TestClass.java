65c23bdc810226fe6a2a124ca97b4f55010384e0
Hrant <harvey.mirijanyan@gmail.com>
2017-05-02 16:55:10 -0700

    Add basicStructures package

---
+++ DS&A/src/basicStructures/TestClass.java
@@ -0,0 +1,7 @@
+package basicStructures;
+
+public class TestClass {
+	public static void main(String[] args){
+		
+	}
+}
---
+++ DS&A/src/basicStructures/myNode.java
@@ -0,0 +1,19 @@
+package basicStructures;
+
+public class myNode {
+	private int val;
+	private myNode next;
+	
+	public myNode(int val){
+		this.val = val;
+		this.next = null;
+	}
+	
+	public myNode getNext(){
+		return this.next;
+	}
+	
+	public void setNext(){
+		
+	}
+}
