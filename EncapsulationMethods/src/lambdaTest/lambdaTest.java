3a699b78c3e4382d02d2f5c20ed2d3a015a19048
Hrant <harvey.mirijanyan@gmail.com>
2017-05-02 14:20:14 -0700

    Pass lamda into other functions

--- EncapsulationMethods/src/lambdaTest/lambdaTest.java
+++ EncapsulationMethods/src/lambdaTest/lambdaTest.java
@@ -1,9 +1,21 @@
 package lambdaTest;
 import java.util.*;
+import java.util.function.*;
 
 public class lambdaTest {
+	
+	
+	public static void print (List<Integer> ints, Predicate<Integer> checker){
+		ints.forEach((Integer animal) -> {
+			if (checker.test(animal)){
+				System.out.println(animal);
+			}
+		});
+	}
+	
+	
 	public static void main(String[] args){
-		System.out.println("lambdas");
+//		System.out.println("lambdas");
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("cat");
 		list.add("hat");
@@ -14,6 +26,11 @@ public class lambdaTest {
 				System.out.println(a);
 			}
 		});
-		
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		list2.add(3);
+		list2.add(5);
+		list2.add(12);
+		list2.add(15);
+		print(list2, a -> a > 6);
 	}
 }
