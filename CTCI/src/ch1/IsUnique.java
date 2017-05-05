040c4b16574245dffc501966632d6fbc3b513c64
Hrant <harvey.mirijanyan@gmail.com>
2017-05-04 19:06:15 -0700

    Add isUnique

--- CTCI/src/ch1/IsUnique.java
+++ CTCI/src/ch1/IsUnique.java
@@ -1,7 +1,23 @@
 package ch1;
+import java.util.*;
 
 public class IsUnique {
-	public boolean isUnique(){
-		return true;
+	public boolean isUnique(String str){
+		String[] myArray = str.split("");
+//		System.out.println(Arrays.toString(str.split("")));
+		HashMap myMap = new HashMap();
+		for(int i = 0; i < myArray.length; i++){
+			String el = myArray[i];
+			if(myMap.containsKey(el)){
+				int val = (int) myMap.get(el);
+				val++;
+				myMap.put(el, val);
+			}else{
+				myMap.put(el, 1);
+			}
+//			myMap.put(myArray[i], 1);
+		}
+		System.out.println(myMap.toString());
+		return myMap.size() == myArray.length ? true : false;
 	}
 }
--- CTCI/src/ch1/Test.java
+++ CTCI/src/ch1/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	
 	public static void main(String[] args){
 		IsUnique myUnique = new IsUnique();
-		
+		System.out.println(myUnique.isUnique("hello"));
+		System.out.println(myUnique.isUnique("cats"));
 	}
 }
