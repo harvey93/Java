c4828df9b859c4a3c3959dc4a8db37afdceb9b85
Hrant <harvey.mirijanyan@gmail.com>
2017-06-02 13:13:15 -0700

    Add get to bst

--- CTCI/src/symbolTables/BST.java
+++ CTCI/src/symbolTables/BST.java
@@ -4,7 +4,16 @@ public class BST {
 	private BSTNode root;
 	
 	public int get(char key){
-		
-		return 0;
+		BSTNode x = this.root;
+		while(x != null){
+			if(key < x.getKey()){
+				x = x.getLeft();
+			} else if (key > x.getKey()){
+				x = x.getRight();
+			} else {
+				return x.getVal();
+			}
+		}
+		return -1;
 	}
 }
--- CTCI/src/symbolTables/BSTNode.java
+++ CTCI/src/symbolTables/BSTNode.java
@@ -10,5 +10,35 @@ public class BSTNode {
 	public BSTNode(char key, int val){
 		this.key = key;
 		this.val = val;
-	}	
+	}
+	
+
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+
+	public BSTNode getRight() {
+		return right;
+	}
+
+	public void setRight(BSTNode right) {
+		this.right = right;
+	}
+	
+	public BSTNode getLeft(){
+		return this.left;
+	}
+
+	public void setLeft(BSTNode left) {
+		this.left = left;
+	}
+	
+	public char getKey(){
+		return this.key;
+	}
+	
 }
